3fdca1872c04f3ecf0445d82fef42cb497015101
YeJi Seo <yejiyeji94@gmail.com>
2015-01-30 21:41:56 +0900

    go home

--- da_capo.py
+++ da_capo.py
@@ -3,99 +3,16 @@ from flask.ext.mysql import MySQL
 from werkzeug.security import check_password_hash, generate_password_hash
 import json
 
-# configuration
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('FLASK EXAMPLE_SETTINGS', silent=True)
-
-mysql = MySQL()
-app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'dlguswn12'
-app.config['MYSQL_DATABASE_DB'] = 'da_capo'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
-
-
-
-def connect_db():
-    return mysql.connect()
-
-
-def init_db():
-    """Creates the database tables."""
-    db = connect_db()
-
-    with app.open_resource('dacapo_wb.sql', mode='r') as f:
-        sql_statements = " ".join(f.readlines())
-        for sql in sql_statements.split(";"):
-            if not sql:
-                cursor = db.cursor()
-                cursor.execute(sql)
-                cursor.close()
-    db.close()
-
-
-def query_db(query, args=(), one=False):
-    """Queries the database and returns a list of dictionaries."""
-    g.db.execute(query, args)
-    print (g.db)
-
-    data = g.db.fetchall()
-    print data
-    rv = [dict((g.db.description[idx][0], value)
-               for idx, value in enumerate(row)) for row in data]
-    return (rv[0] if rv else None) if one else rv
-
-@app.before_request
-def before_request():
-    print ("Start Application")
-    db_connect = connect_db()
-    db_connect.autocommit(1)
-
-    g.db = db_connect.cursor()
-    print type(g.db)
-
 
 @app.route('/')
 def base_test():
     return render_template('base.html')
 
 @app.route('/login')
+
 def login():
     return render_template('login.html')
 
-@app.route('/check_login',  methods=["POST"])
-def login_check():
-    if request.method == "POST":
-        id = request.form['id']
-        password = request.form['password']
-
-        user = query_db('''select * from User where studentId = %s''', [id], one=True)
-        print user
-        if password == user['UserPassword']:
-            session['user_id'] = user['studentId']
-            return "OK"
-        else:
-            error = 'Invalid password'
-            return  render_template('login.html', error=error)
-
-
-    # user = query_db('''select * from user where email = %s''', [request.form['email']], one=True)
-    #
-    # if user is None:
-    #     error = 'Invalid username'
-    # elif not check_password_hash(user['password'], request.form['password']):
-    #     error = 'Invalid password'
-    # else:
-    #     session['user_id'] = user['email']
-    #     return redirect(url_for('server_list'))
-
-    # return  render_template('login.html', error=error)
-
-    return id + " "  + password
-
-
 if __name__ == "__main__":
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(debug=True)
+    app.run()
