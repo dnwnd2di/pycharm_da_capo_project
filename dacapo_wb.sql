a55531e6ab33d31a7fc57e67ad7258d16dab045f
Hyeonju Lee <l67378810@gmail.com>
2015-01-30 21:36:42 +0900

    happy!

--- dacapo_wb.sql
+++ dacapo_wb.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `ClassroomNumber` (
+CREATE TABLE if not exists `ClassroomNumber` (
   `RoomNumber` int(11) NOT NULL,
   PRIMARY KEY (`RoomNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `Master` (
+CREATE TABLE if not exists `Master` (
   `Number` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
   `Status` varchar(45) NOT NULL,
   `MasterReason` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `Reservation` (
+CREATE TABLE if not exists `Reservation` (
   `Number` int(11) NOT NULL,
   `StudentID` int(11) NOT NULL,
   `Date` date NOT NULL,
@@ -22,14 +22,14 @@ CREATE TABLE `Reservation` (
   PRIMARY KEY (`Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `ReservationLeader` (
+CREATE TABLE if not exists `ReservationLeader` (
   `Number` int(11) NOT NULL,
   `LeaderID` int(11) NOT NULL,
   `Date` date NOT NULL,
   PRIMARY KEY (`Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `ReservationMember` (
+CREATE TABLE if not exists `ReservationMember` (
   `Number` int(11) NOT NULL,
   `LeaderNumber` int(11) NOT NULL,
   `MemberID` int(11) NOT NULL,
@@ -37,13 +37,13 @@ CREATE TABLE `ReservationMember` (
   PRIMARY KEY (`Number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `Students` (
+CREATE TABLE if not exists `Students` (
   `StudentID` int(11) NOT NULL,
   `StudentName` varchar(45) NOT NULL,
   PRIMARY KEY (`StudentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `User` (
+CREATE TABLE if not exists `User` (
   `StudentID` int(11) NOT NULL,
   `UserName` varchar(45) NOT NULL,
   `UserPassword` varchar(45) NOT NULL,
